61af5352b5796dcd5c726015936efba2e39d50fe
Андреев Александр <aandreev@synterramedia.ru>
2024-05-10 23:40:20 +0300

    Added GET-parameter queryType to HTTP-request

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"bytes"
 	"encoding/base64"
+	"encoding/binary"
 	"flag"
 	"fmt"
 	"io"
@@ -12,6 +13,100 @@ import (
 	"net/url"
 	"strings"
 )
+
+func QueryType(qtype uint16) (string, bool) {
+	types := map[uint16]string{
+		0:     "None",
+		1:     "A",
+		2:     "NS",
+		3:     "MD",
+		4:     "MF",
+		5:     "CNAME",
+		6:     "SOA",
+		7:     "MB",
+		8:     "MG",
+		9:     "MR",
+		10:    "NULL",
+		12:    "PTR",
+		13:    "HINFO",
+		14:    "MINFO",
+		15:    "MX",
+		16:    "TXT",
+		17:    "RP",
+		18:    "AFSDB",
+		19:    "X25",
+		20:    "ISDN",
+		21:    "RT",
+		23:    "NSAPPTR",
+		24:    "SIG",
+		25:    "KEY",
+		26:    "PX",
+		27:    "GPOS",
+		28:    "AAAA",
+		29:    "LOC",
+		30:    "NXT",
+		31:    "EID",
+		32:    "NIMLOC",
+		33:    "SRV",
+		34:    "ATMA",
+		35:    "NAPTR",
+		36:    "KX",
+		37:    "CERT",
+		39:    "DNAME",
+		41:    "OPT",
+		42:    "APL",
+		43:    "DS",
+		44:    "SSHFP",
+		45:    "IPSECKEY",
+		46:    "RRSIG",
+		47:    "NSEC",
+		48:    "DNSKEY",
+		49:    "DHCID",
+		50:    "NSEC3",
+		51:    "NSEC3PARAM",
+		52:    "TLSA",
+		53:    "SMIMEA",
+		55:    "HIP",
+		56:    "NINFO",
+		57:    "RKEY",
+		58:    "TALINK",
+		59:    "CDS",
+		60:    "CDNSKEY",
+		61:    "OPENPGPKEY",
+		62:    "CSYNC",
+		63:    "ZONEMD",
+		64:    "SVCB",
+		65:    "HTTPS",
+		99:    "SPF",
+		100:   "UINFO",
+		101:   "UID",
+		102:   "GID",
+		103:   "UNSPEC",
+		104:   "NID",
+		105:   "L32",
+		106:   "L64",
+		107:   "LP",
+		108:   "EUI48",
+		109:   "EUI64",
+		249:   "TKEY",
+		250:   "TSIG",
+		251:   "IXFR",
+		252:   "AXFR",
+		253:   "MAILB",
+		254:   "MAILA",
+		255:   "ANY",
+		256:   "URI",
+		257:   "CAA",
+		258:   "AVC",
+		260:   "AMTRELAY",
+		32768: "TA",
+		32769: "DLV",
+		65535: "Reserved",
+	}
+
+	t, ok := types[qtype]
+	return t, ok
+}
 
 func main() {
 
@@ -55,8 +150,15 @@ func main() {
 			continue
 		}
 
-		// URL пользовательского DNS-over-HTTPS сервера
-		serverURL := *doh
+		// Извлекаем тип запроса DNS
+		queryTypeBytes := binary.BigEndian.Uint16(buf[n-4 : n-2])
+		queryType, ok := QueryType(queryTypeBytes)
+
+		// Задаем параметры запроса
+		params := url.Values{}
+		if ok {
+			params.Add("queryType", queryType)
+		}
 
 		// Создаём запрос с заголовками X-Forwarded-For, X-Real-IP и другие
 		var req *http.Request
@@ -66,14 +168,10 @@ func main() {
 			dnsRequestBase64 := base64.RawURLEncoding.EncodeToString(buf[:n])
 			dnsRequestBase64 = strings.TrimRight(dnsRequestBase64, "=")
 
-			// Задаем параметры запроса
-			params := url.Values{}
+			// Добавляем параметры запроса
 			params.Add("dns", dnsRequestBase64)
 
-			// Формируем URL запроса для GET запроса
-			requestURL := fmt.Sprintf("%s?%s", serverURL, params.Encode())
-
-			req, errReq = http.NewRequest("GET", requestURL, nil)
+			req, errReq = http.NewRequest("GET", serverURL, nil)
 		} else {
 			req, errReq = http.NewRequest("POST", serverURL, bytes.NewBuffer(buf[:n]))
 
@@ -83,6 +181,11 @@ func main() {
 			fmt.Println("Error creating request:", err)
 			continue
 		}
+
+		// Добавляем параметры запроса
+		req.URL.RawQuery = params.Encode()
+
+		// Добавляем заголовки
 		req.Header.Set("X-Forwarded-For", addr.IP.String())
 		req.Header.Set("X-Real-IP", addr.IP.String())
 		req.Header.Set("Host", localAddr.String())
